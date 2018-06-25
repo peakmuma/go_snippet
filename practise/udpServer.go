763dbc95e9545588f96e968fe757ee1bb9ec6e3a
gaolei <gaolei@sunlands.com>
2018-06-25 17:40:50 +0800

    add udpserver

---
+++ practise/udpServer.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func startUDPServer (port int) {
+	udpConn, err := net.ListenUDP("udp", &net.UDPAddr{IP: net.ParseIP("0.0.0.0"), Port: port})
+	check(err)
+	fmt.Println ("UDP Server start at port: ", port)
+	data := make([]byte, 1024)
+	for {
+		n, remoteAddr, err := udpConn.ReadFromUDP(data)
+		check(err)
+		fmt.Printf("[%s] %s\n", remoteAddr, data[:n])
+	}
+
+}
